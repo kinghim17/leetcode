8c38160821a5c44c176841d14a6a2eb4314976c6
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-06-12 17:36:07 +0530

    Add files via upload

---
+++ sliding-window/Count Subarrays Where Max Element Appears at Least K Time.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxE = *max_element(begin(nums), end(nums));
+        
+        int n = nums.size();
+        int i = 0, j = 0;
+        
+        long long result = 0;
+        int countMax = 0;
+        
+        while(j < n) {
+            if(nums[j] == maxE) {
+                countMax++;
+            }
+            
+            while(countMax >= k) {
+                result += n-j;
+                
+                if(nums[i] == maxE) {
+                    countMax--;
+                }
+                i++;
+            }
+            j++;
+        }
+        
+        return result;
+    }
+};
+
+//approach 2
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxE = *max_element(begin(nums), end(nums));
+        
+        int n = nums.size();
+        
+        long long result = 0;
+
+        vector<int> maxIndices;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == maxE) {
+                maxIndices.push_back(i);
+            }
+            
+            int size = maxIndices.size();
+            if(size >= k) {
+                int last_i = maxIndices[size-k];
+                result += last_i+1;
+            }
+        }
+        
+        return result;
+    }
+};
