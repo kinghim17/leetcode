f475603ce3530505899b7f7730e2b4a7217228ef
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2024-07-03 07:17:13 +0530

    Update Relative Sort Array.cpp

--- vector/Relative Sort Array.cpp
+++ vector/Relative Sort Array.cpp
@@ -36,7 +36,7 @@ public:
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int, int> mp;
+        ordered_map<int, int> mp;
 
         for(int i = 0; i < arr2.size(); i++) {
             mp[arr2[i]] = i;
